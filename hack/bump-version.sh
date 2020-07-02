e938478140d7c7ca1d912c6b9a1ca8ae2f14acf7
radtriste <tradisso@redhat.com>
2020-07-02 12:16:07 +0200

    KOGITO-2290/2291 Setup stages for release

--- hack/bump-version.sh
+++ hack/bump-version.sh
@@ -30,6 +30,7 @@ fi
 
 sed -i "s/$old_version/$new_version/g" cmd/kogito/version/version.go README.md version/version.go deploy/operator.yaml deploy/olm-catalog/kogito-operator/kogito-operator.package.yaml hack/go-build.sh hack/go-vet.sh .osdk-scorecard.yaml
 operator-sdk generate csv --apis-dir ./pkg/apis/apps/v1alpha1 --verbose --csv-version "$new_version" --from-version "$old_version"  --update-crds --operator-name kogito-operator
+sed -i "s|operated-by: kogito-operator.*|operated-by: kogito-operator.${new_version}|g" deploy/olm-catalog/kogito-operator/manifests/kogito-operator.clusterserviceversion.yaml
 
 # rewrite test default config, all other configuration into the file will be overridden
 test_config_file="test/.default_config"
@@ -43,9 +44,9 @@ else
     branch="${image_version}.x"
 fi
 
-rm ${test_config_file}
-echo "tests.build-image-version=${image_version}" >> ${test_config_file}
-echo "tests.services-image-version=${image_version}" >> ${test_config_file}
-echo "tests.examples-ref=${branch}" >> ${test_config_file}
+sed -i "s|tests.build-image-version=.*|tests.build-image-version=${image_version}|g" ${test_config_file}
+sed -i "s|tests.services-image-version=.*|tests.services-image-version=${image_version}|g" ${test_config_file}
+sed -i "s|tests.runtime-application-image-version=.*|tests.runtime-application-image-version=${image_version}|g" ${test_config_file}
+sed -i "s|tests.examples-ref=.*|tests.examples-ref=${branch}|g" ${test_config_file}
 
 echo "Version bumped from $old_version to $new_version"
